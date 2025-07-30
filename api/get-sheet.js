6f1f7c853b11c6b1e6b50b79e932bc953619f48d
Dawid1988-maker <z.uzytkownik@wp.pl>
2025-07-30 14:52:16 +0200

    Update get-sheet.js

--- api/get-sheet.js
+++ api/get-sheet.js
@@ -1,28 +1,29 @@
-import { google } from 'googleapis';
-import dotenv from 'dotenv';
+import { google } from "googleapis";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default async function handler(req, res) {
   try {
     const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY),
-      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+      credentials: JSON.parse(process.env.GOOGLE_SERVICE_KEY),
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
     });
 
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
     const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: '1ADDM_lOWCMewrxPBta1q8cHhKIA0ET2FecUUdXqpdjQ',
-      range: 'Arkusz1!A1:Z1000',
+      spreadsheetId: process.env.ID_ARKUSZA_KALKULACYJNEJ,
+      range: "Arkusz1!A1:Z1000",
     });
 
     res.status(200).json({ data: response.data.values });
   } catch (error) {
-    console.error('Błąd pobierania danych z arkusza:', error);
-    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' });
+    console.error("Błąd pobierania danych z arkusza:", error);
+    res.status(500).json({ error: "Wystąpił błąd podczas pobierania danych." });
   }
 }
 
+   
 
 
