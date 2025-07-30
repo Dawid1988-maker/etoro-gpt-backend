884a2260b52248a0b5f5545d7daf8099d8756b25
Dawid1988-maker <z.uzytkownik@wp.pl>
2025-07-30 11:21:45 +0200

    Update test.ts

--- api/test.ts
+++ api/test.ts
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Backend działa ✅' });
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ message: 'Backend działa ✅' })
 }
